447efe22b20e973786247e89c0480a65ca1a99a5
Aurelie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2021-09-17 10:30:02 +0000

    version 0.0-3

---
+++ README.md
@@ -0,0 +1,21 @@
+[![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/nlsMicrobio)](https://cran.r-project.org/package=nlsMicrobio)
+[![CRAN Downloads](https://cranlogs.r-pkg.org/badges/nlsMicrobio)](https://cran.r-project.org/package=nlsMicrobio)
+[![R-CMD-check](https://github.com/aursiber/nlsMicrobio/workflows/R-CMD-check/badge.svg)](https://github.com/aursiber/nlsMicrobio/actions)
+
+The stable version of `nlsMicrobio` can be installed from CRAN using:
+```r
+install.packages("nlsMicrobio")
+```
+
+The development version of `nlsMicrobio` can be installed from GitHub (`remotes` needed):
+```r
+if (!requireNamespace("remotes", quietly = TRUE))
+   install.packages("remotes")
+   
+remotes::install_github("aursiber/nlsMicrobio")
+``` 
+
+Finally load the package in your current R session with the following R command:
+```r
+library(nlsMicrobio)
+```
--- inst/doc/nlsMicrobio_vignette.R
+++ inst/doc/nlsMicrobio_vignette.R
@@ -1,133 +1,134 @@
-### R code from vignette source 'nlstools_vignette.Rnw'
+### R code from vignette source 'nlsMicrobio_vignette.Rnw'
 
 ###################################################
-### code chunk number 1: nlstools_vignette.Rnw:24-25
+### code chunk number 1: nlsMicrobio_vignette.Rnw:26-27
+###################################################
+library(nlstools)
+
+
+###################################################
+### code chunk number 2: nlsMicrobio_vignette.Rnw:44-48
 ###################################################
 library(nlsMicrobio)
-
-
-###################################################
-### code chunk number 2: nlstools_vignette.Rnw:41-44
-###################################################
 data(survivalcurve2)
 preview(formula=mafart, data=survivalcurve2,
-start=list(p = 1, delta = 1, LOG10N0 = 7))
+        start=list(p = 1, delta = 1, LOG10N0 = 7))
 
 
 ###################################################
-### code chunk number 3: nlstools_vignette.Rnw:49-51
-###################################################
-preview(formula=mafart, data=survivalcurve2,
-start=list(p = 1, delta = 10, LOG10N0 = 7))
-
-
-###################################################
-### code chunk number 4: nlstools_vignette.Rnw:56-58
+### code chunk number 3: nlsMicrobio_vignette.Rnw:53-55
 ###################################################
 preview(formula=mafart, data=survivalcurve2,
-start=list(p = 2, delta = 10, LOG10N0 = 7.5))
+        start=list(p = 1, delta = 10, LOG10N0 = 7))
 
 
 ###################################################
-### code chunk number 5: nlstools_vignette.Rnw:67-69
+### code chunk number 4: nlsMicrobio_vignette.Rnw:60-62
+###################################################
+preview(formula=mafart, data=survivalcurve2,
+        start=list(p = 2, delta = 10, LOG10N0 = 7.5))
+
+
+###################################################
+### code chunk number 5: nlsMicrobio_vignette.Rnw:71-73
 ###################################################
 nlsmaf <- nls(mafart, survivalcurve2, list(p = 2, delta = 10, LOG10N0 = 7.5))
 plotfit(nlsmaf,smooth=TRUE)
 
 
 ###################################################
-### code chunk number 6: nlstools_vignette.Rnw:74-76
+### code chunk number 6: nlsMicrobio_vignette.Rnw:78-80
 ###################################################
 model <- LOG10N ~ LOG10N0 - (t/delta)^p
 nlsmaf <- nls(model, survivalcurve2, list(p = 2, delta = 10, LOG10N0 = 7.5))
 
 
 ###################################################
-### code chunk number 7: nlstools_vignette.Rnw:81-87
+### code chunk number 7: nlsMicrobio_vignette.Rnw:85-91
 ###################################################
 data(ross)
 d6<-subset(ross, select = c(T, pH, aw, sqrtmumax))
 nls6 <- nls(cpm_T_pH_aw, d6, list(muopt = 2, Tmin = 4,
-Topt = 40, Tmax = 49,pHmin = 4, pHopt = 6.5, pHmax = 9,
-awmin = 0.95, awopt = 0.995))
+                                  Topt = 40, Tmax = 49,pHmin = 4, pHopt = 6.5, pHmax = 9,
+                                  awmin = 0.95, awopt = 0.995))
 plotfit(nls6, variable = 1)
 
 
 ###################################################
-### code chunk number 8: nlstools_vignette.Rnw:89-90
+### code chunk number 8: nlsMicrobio_vignette.Rnw:93-94
 ###################################################
 plotfit(nls6, variable = 2)
 
 
 ###################################################
-### code chunk number 9: nlstools_vignette.Rnw:95-96
+### code chunk number 9: nlsMicrobio_vignette.Rnw:99-100
 ###################################################
 overview(nlsmaf)
 
 
 ###################################################
-### code chunk number 10: nlstools_vignette.Rnw:105-107
+### code chunk number 10: resmaf
 ###################################################
 resmaf<-nlsResiduals(nlsmaf)
 plot(resmaf)
 
 
 ###################################################
-### code chunk number 11: nlstools_vignette.Rnw:114-115
+### code chunk number 11: nlsMicrobio_vignette.Rnw:119-120
 ###################################################
 test.nlsResiduals(resmaf)
 
 
 ###################################################
-### code chunk number 12: nlstools_vignette.Rnw:127-128
+### code chunk number 12: nlsMicrobio_vignette.Rnw:132-133
 ###################################################
 contmaf <- nlsContourRSS(nlsmaf)
 
 
 ###################################################
-### code chunk number 13: nlstools_vignette.Rnw:131-132
+### code chunk number 13: nlsMicrobio_vignette.Rnw:136-137
 ###################################################
 plot(contmaf, col=FALSE, nlev=10)
 
 
 ###################################################
-### code chunk number 14: nlstools_vignette.Rnw:146-147
+### code chunk number 14: nlsMicrobio_vignette.Rnw:151-152
 ###################################################
 rcmaf <- nlsConfRegions(nlsmaf, length=500, exp=1)
 
 
 ###################################################
-### code chunk number 15: nlstools_vignette.Rnw:149-150
+### code chunk number 15: nlsMicrobio_vignette.Rnw:154-155
 ###################################################
 plot(rcmaf, bounds=T)
 
 
 ###################################################
-### code chunk number 16: nlstools_vignette.Rnw:155-156
+### code chunk number 16: nlsMicrobio_vignette.Rnw:160-161
 ###################################################
 rcmaf <- nlsConfRegions(nlsmaf, length=500, exp=2)
 
 
 ###################################################
-### code chunk number 17: nlstools_vignette.Rnw:159-160
+### code chunk number 17: nlsMicrobio_vignette.Rnw:164-165
 ###################################################
 plot(rcmaf,bounds=T)
 
 
 ###################################################
-### code chunk number 18: nlstools_vignette.Rnw:165-166
+### code chunk number 18: nlsMicrobio_vignette.Rnw:170-171
 ###################################################
 rcmaf <- nlsConfRegions(nlsmaf, length=2000, exp=2)
 
 
 ###################################################
-### code chunk number 19: nlstools_vignette.Rnw:169-170
+### code chunk number 19: nlsMicrobio_vignette.Rnw:174-175
 ###################################################
 plot(rcmaf, bounds=F)
 
 
 ###################################################
-### code chunk number 20: nlstools_vignette.Rnw:177-180
+### code chunk number 20: nlsMicrobio_vignette.Rnw:182-185
 ###################################################
 plot(rcmaf$cr[,1], rcmaf$cr[,3], pch=16, xlab='p', ylab='LOG10N')
 contour(contmaf$seqPara[, 1], contmaf$seqPara[, 3], contmaf$lrss[[2]], 
@@ -135,37 +136,37 @@ contour(contmaf$seqPara[, 1], contmaf$seqPara[, 3], contmaf$lrss[[2]],
 
 
 ###################################################
-### code chunk number 21: nlstools_vignette.Rnw:190-191
+### code chunk number 21: nlsMicrobio_vignette.Rnw:195-196
 ###################################################
 jackmaf <- nlsJack(nlsmaf)
 
 
 ###################################################
-### code chunk number 22: nlstools_vignette.Rnw:193-194
+### code chunk number 22: nlsMicrobio_vignette.Rnw:198-199
 ###################################################
 summary(jackmaf)
 
 
 ###################################################
-### code chunk number 23: nlstools_vignette.Rnw:199-200
+### code chunk number 23: nlsMicrobio_vignette.Rnw:204-205
 ###################################################
 plot(jackmaf)
 
 
 ###################################################
-### code chunk number 24: nlstools_vignette.Rnw:207-208
+### code chunk number 24: nlsMicrobio_vignette.Rnw:213-214
 ###################################################
 boomaf <- nlsBoot(nlsmaf, niter=2000) 
 
 
 ###################################################
-### code chunk number 25: nlstools_vignette.Rnw:211-212
+### code chunk number 25: nlsMicrobio_vignette.Rnw:217-218
 ###################################################
 summary(boomaf)
 
 
 ###################################################
-### code chunk number 26: nlstools_vignette.Rnw:217-218
+### code chunk number 26: nlsMicrobio_vignette.Rnw:223-224
 ###################################################
 plot(boomaf, type="pairs")
 
